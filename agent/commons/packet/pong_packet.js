e36147967aaa9e999f702bd280bfcaaa7563a2de
peaksnail <peaksnail@gmail.com>
2016-08-27 00:58:24 +0800

    add pong_packet

---
+++ agent/commons/packet/pong_packet.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * send packet class
+ *
+ * Date: 2016-08-24 21:30
+ * Author: psnail
+ *
+ */
+
+'use strict';
+var BasicPacket = require('./basic_packet.js');
+var Objects = require('../../utils/objects.js');
+var PacketType = require('./packet_type.js');
+var FixedBuffer = require('../buffer/fixed_buffer.js');
+var PayloadPacket = require('../../utils/payload_packet.js');
+
+var PongPacket = function () {
+};
+
+Objects.extends(false, PongPacket, BasicPacket);
+
+PongPacket.prototype.getPacketType = function () {
+    return PacketType.CONTROL_PONG;
+};
+
+PongPacket.prototype.toBuffer = function () {
+    var buffer = new FixedBuffer(2);
+    buffer.writeShort(PacketType.CONTROL_PONG)
+    return buffer.getBuffer();
+};
+
+
+module.exports = PongPacket;
